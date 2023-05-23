d55ef5e7a290d04bbd9d6d7549718d707be9155a
Seungwoo Oh <sw6016@hanmail.net>
2023-05-23 21:52:30 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -0,0 +1,23 @@
+package com.fastcampus.ch2;
+
+// 년월일을 입력하면 요일을 알려주는 원격 프로그램
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class YoilTeller {
+    @RequestMapping("/getYoil")
+    public String main(HttpServletRequest request) {
+        String year = request.getParameter("year");
+        String month = request.getParameter("month");
+        String day = request.getParameter("day");
+
+        System.out.println("year = " + year);
+        System.out.println("month = " + month);
+        System.out.println("day = " + day);
+        return "Monday";
+    }
+}
